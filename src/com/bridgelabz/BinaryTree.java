b6e93c8a841d06a30832e8b3ab9beeebea333d79
SUDIPP22 <panja370@gmail.com>
2021-09-10 01:31:35 +0530

    Added : Welcome Message

---
+++ src/com/bridgelabz/BinaryTree.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To implement Binary Search Tree Operations
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-09-08
+ */
+public class BinaryTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Binary Tree Program");
+    }
+}
